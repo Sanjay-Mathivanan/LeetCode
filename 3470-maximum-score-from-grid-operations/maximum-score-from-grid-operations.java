363af66d2f992bd7f2966cc59a2e097feef5b60b
Sanjay M <151824725+Sanjay-Mathivanan@users.noreply.github.com>
2026-04-29 21:01:22 +0530

    Time: 49 ms (82.14%) | Memory: 48.1 MB (82.14%) - LeetSync

---
+++ 3470-maximum-score-from-grid-operations/maximum-score-from-grid-operations.java
@@ -0,0 +1,80 @@
+class Solution {
+    public long maximumScore(int[][] grid) {
+        final int n = grid.length;
+
+        // prefix[j][i] = sum of first i cells in column j
+        long[][] prefix = new long[n][n + 1];
+
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < n; ++i) {
+                prefix[j][i + 1] = prefix[j][i] + grid[i][j];
+            }
+        }
+
+        // prevPick[i]:
+        // best score where previous column height = i
+        long[] prevPick = new long[n + 1];
+
+        // prevSkip[i]:
+        // best score where previous boundary already handled
+        long[] prevSkip = new long[n + 1];
+
+        for (int j = 1; j < n; ++j) {
+
+            long[] currPick = new long[n + 1];
+            long[] currSkip = new long[n + 1];
+
+            for (int curr = 0; curr <= n; ++curr) {
+
+                for (int prev = 0; prev <= n; ++prev) {
+
+                    if (curr > prev) {
+
+                        // contribution from previous column
+                        long score =
+                                prefix[j - 1][curr]
+                              - prefix[j - 1][prev];
+
+                        currPick[curr] = Math.max(
+                                currPick[curr],
+                                prevSkip[prev] + score
+                        );
+
+                        currSkip[curr] = Math.max(
+                                currSkip[curr],
+                                prevSkip[prev] + score
+                        );
+
+                    } else {
+
+                        // contribution from current column
+                        long score =
+                                prefix[j][prev]
+                              - prefix[j][curr];
+
+                        currPick[curr] = Math.max(
+                                currPick[curr],
+                                prevPick[prev] + score
+                        );
+
+                        currSkip[curr] = Math.max(
+                                currSkip[curr],
+                                prevPick[prev]
+                        );
+                    }
+                }
+            }
+
+            prevPick = currPick;
+            prevSkip = currSkip;
+        }
+
+        long ans = 0;
+
+        for (long x : prevPick) {
+            ans = Math.max(ans, x);
+        }
+
+        return ans;
+    }
+}
