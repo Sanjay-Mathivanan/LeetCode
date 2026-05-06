bceb966bde7b6d1117e3100fe445ca4ea6216530
Sanjay M <151824725+Sanjay-Mathivanan@users.noreply.github.com>
2026-05-06 21:44:21 +0530

    Time: 8 ms (59.38%) | Memory: 124.9 MB (43.27%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,39 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+
+        // Step 1: Apply gravity in each row
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                // Obstacle resets the position
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                }
+
+                // Move stone to the rightmost possible position
+                else if (boxGrid[i][j] == '#') {
+
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][empty] = '#';
+
+                    empty--;
+                }
+            }
+        }
+
+        // Step 2: Rotate matrix 90 degrees clockwise
+        char[][] result = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return result;
+    }
+}
