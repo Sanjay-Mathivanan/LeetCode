33f9594f8734fc88d867ec098d9f76e858584f30
Sanjay M <151824725+Sanjay-Mathivanan@users.noreply.github.com>
2026-06-05 14:03:17 +0530

    Time: 25 ms (81.75%) | Memory: 46.7 MB (20.44%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,39 +1,21 @@
 class Solution {
-
     public int totalWaviness(int num1, int num2) {
-
         int ans = 0;
+        for(int num=num1; num<=num2; num++){
 
-        for (int num = num1; num <= num2; num++) {
-            ans += getWaviness(num);
-        }
-
-        return ans;
-    }
-
-    private int getWaviness(int num) {
-
-        String s = String.valueOf(num);
-
-        if (s.length() < 3) {
-            return 0;
-        }
-
-        int waviness = 0;
-
-        for (int i = 1; i < s.length() - 1; i++) {
-
-            char prev = s.charAt(i - 1);
-            char cur = s.charAt(i);
-            char next = s.charAt(i + 1);
-
-            if ((cur > prev && cur > next) ||
-                (cur < prev && cur < next)) {
-
-                waviness++;
+            String s= String.valueOf(num);
+            for(int i=1;i<s.length()-1;i++){
+                char left = s.charAt(i-1);
+                char mid =s.charAt(i);
+                char right=s.charAt(i+1);
+                if((mid>left && mid>right || mid<left && mid<right)){
+                    ans++;
+                }
             }
-        }
 
-        return waviness;
+        }
+        return ans;
+
+        
     }
 }
